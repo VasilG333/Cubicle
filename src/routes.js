77689af833f6874d16da3e004fd5e76d0e60670e
VasilG333 <107360902+VasilG333@users.noreply.github.com>
2022-06-12 19:55:07 +0300

    Added about page

--- src/routes.js
+++ src/routes.js
@@ -3,5 +3,6 @@ const router = express.Router()
 const homeController = require('./controllers/homeController')
 
 router.get('/', homeController.index)
+router.get('/about', homeController.about)
 
 module.exports = router;
