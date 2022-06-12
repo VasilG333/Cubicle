79e8a289d43ddd60de89759305223f18b5656dc0
VasilG333 <107360902+VasilG333@users.noreply.github.com>
2022-06-12 19:53:57 +0300

    Added views, css, homepage working

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 const express = require('express')
+const hbs = require('express-handlebars')
+const router = require('./routes')
 
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send('Hello')
-})
+app.use('/static', express.static('static'))
+
+app.engine('.hbs', hbs.engine({extname: '.hbs'}));
+app.set('view engine', '.hbs');
+app.set('views', './src/views');
+
+app.use(router)
 
 app.listen(5000, () => console.log('Server started on port 5000...'))
